2084a793588090e2d8b7ce49ec0a897339b6b869
YaroslavlProhodimec <yaroslav.muratov.97@inbox.ru>
2025-10-25 23:19:28 +0300

    Realise busket

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import AppHeader from '@/components/AppHeader.vue'
 import SneakersCardList from '@/components/SneakersCardList.vue'
+import DrawerSide from '@/components/DrawerSide.vue'
 </script>
 
 <template>
   <div class="w-4/5 m-auto h-screen rounded-xl shadow-xl mt-14">
     <AppHeader />
+    <DrawerSide />
     <div class="p-10">
     <h2 class="text-3xl fond-bold mb-8">Все кроссовки</h2>
     <SneakersCardList/>

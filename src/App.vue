26de39b6412780350ed12198f2162f13c2afb75d
YaroslavlProhodimec <yaroslav.muratov.97@inbox.ru>
2025-10-26 01:40:54 +0300

    Add select  ui

--- src/App.vue
+++ src/App.vue
@@ -7,21 +7,28 @@ import SneakersCardList from '@/components/SneakersCardList.vue'
 <template>
   <div class="w-4/5 m-auto h-screen rounded-xl shadow-xl mt-14">
     <AppHeader />
-<!--    <DrawerSide />-->
+    <!--    <DrawerSide />-->
     <div class="p-10">
-      <div class="flex justify-between">
+      <div class="flex justify-between items-center">
         <h2 class="text-3xl fond-bold mb-8">Все кроссовки</h2>
-        <select name="" id="">
-          <option value=""></option>
-          <option value=""></option>
-          <option value=""></option>
-        </select>
-        <div class="relative">
-          <img src="/search.svg" class="absolute left-4 top-3" alt="">
-          <input class="border rounded-md py-2 pl-11 pr-4 outline-none focus:border-gray-400" type="text" placeholder="Поиск">
+
+        <div class="flex gap-4">
+          <select class="py-2 px-3 border rounded-md outline-none">
+            <option value="">По названию</option>
+            <option value="">По цене (дешевые)</option>
+            <option value="">По цене (дорогие)</option>
+          </select>
+          <div class="relative">
+            <img src="/search.svg" class="absolute left-4 top-3" alt="" />
+            <input
+              class="border rounded-md py-2 pl-11 pr-4 outline-none focus:border-gray-400"
+              type="text"
+              placeholder="Поиск"
+            />
+          </div>
         </div>
       </div>
-    <SneakersCardList/>
+      <SneakersCardList />
     </div>
   </div>
 </template>
